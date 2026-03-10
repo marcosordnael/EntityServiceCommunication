3c7561eada5c3aee8680b4764b24fc1acb8f7436
macbook <macbook@MacBook-Pro-de-Marcos.local>
2026-03-10 16:34:01 -0300

    Parte 2 - implementação de aplicativo (videoaula)

--- app/src/main/java/br/edu/ifsp/scl/sdm/entityservicecommunication/IncrementService.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/entityservicecommunication/IncrementService.kt
@@ -2,13 +2,40 @@ package br.edu.ifsp.scl.sdm.entityservicecommunication
 
 import android.app.Service
 import android.content.Intent
+import android.os.Handler
+import android.os.HandlerThread
 import android.os.IBinder
+import android.os.Looper
+import android.os.Message
 
 class IncrementService : Service() {
 
+    private inner class IncrementHandler(looper: Looper) : Handler(looper) {
+        override fun handleMessage(msg: Message) {
+            super.handleMessage(msg)
+            msg.data.getInt("VALUE").also {
+                InterEntityCommunication.valueLiveData.postValue(it + 1)
+                Intent ("INCREMENT_VALUE_ACTION").apply {
+                    putExtra("VALUE", it + 1)
+                    sendBroadcast(this)
+                }
+            }
+            stopSelf()
+        }
+    }
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        intent?.getIntExtra("VALUE", -1)?.also {
-            InterEntityCommunication.valueLiveData.postValue(it + 1)
+        intent?.getIntExtra("VALUE", -1)?.also { value ->
+            HandlerThread("IncrementService").apply {
+                start()
+                IncrementHandler(looper).apply {
+                    obtainMessage().apply {
+                        data.putInt("VALUE", value)
+                        sendMessage(this)
+                    }
+                }
+            }
+
         }
         return START_NOT_STICKY
     }
